95559d2baf55273d4121903ae37d3256b4c22b78
Nischal Pandey <162779921+Nischal-pandey@users.noreply.github.com>
2024-08-31 11:42:47 +0530

    updateed

--- ecommerce.js
+++ ecommerce.js
@@ -1,12 +1,50 @@
-// script.js
+// Select individual navigation links
+const homeLink = document.querySelector('nav ul li a[href="#"]');
+const aboutLink = document.querySelector('nav ul li a[href="#about"]');
+const productsLink = document.querySelector('nav ul li a[href="#products"]');
+const blogLink = document.querySelector('nav ul li a[href="#blog"]');
+const clientLink = document.querySelector('nav ul li a[href="#client"]');
+const careerLink = document.querySelector('nav ul li a[href="#career"]');
+const contactLink = document.querySelector('nav ul li a[href="#contact"]');
+const downloadSoftwareLink = document.querySelector('nav ul li a[href="#download-software"]');
 
-// Add event listeners to the "Buy Now" buttons
-const buyNowButtons = document.querySelectorAll('.sneakers-item button');
+// Add onclick function to each navigation link
+homeLink.onclick = function() {
+  // Add your code here for the Home link
+  console.log("Home link clicked");
+};
 
-buyNowButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        // You can add your logic here when a "Buy Now" button is clicked
-        // For example, you could redirect the user to a product page
-        alert('Buy Now button clicked!');
-    });
-});
+aboutLink.onclick = function() {
+  // Add your code here for the About link
+  console.log("About link clicked");
+};
+
+productsLink.onclick = function() {
+  // Add your code here for the Products link
+  console.log("Products link clicked");
+};
+
+blogLink.onclick = function() {
+  // Add your code here for the Blog link
+  console.log("Blog link clicked");
+};
+
+clientLink.onclick = function() {
+  // Add your code here for the Client link
+  console.log("Client link clicked");
+};
+
+careerLink.onclick = function() {
+  // Add your code here for the Career link
+  console.log("Career link clicked");
+};
+
+contactLink.onclick = function() {
+  // Add your code here for the Contact link
+  console.log("Contact link clicked");
+};
+
+downloadSoftwareLink.onclick = function() {
+  // Add your code here for the Download Software link
+  console.log("Download Software link clicked");
+};
